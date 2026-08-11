342cb636367202faff44f2f15661c03dcf849fed
dariusehssan <dariusehssan@gmail.com>
2026-08-11 21:59:50 +0100

    Update LabelForm.jsx

--- frontend/src/LabelForm.jsx
+++ frontend/src/LabelForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Labels.css";
 
-const API_URL = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://data-to-graph-web-automator.onrender.com";
 
 const LabelForm = ({ existingLabel = {}, updateCallback }) => {
     const [xlabel, setXLabel] = useState(existingLabel.XLabel || "");
