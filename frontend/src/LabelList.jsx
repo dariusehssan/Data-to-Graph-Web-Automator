9b3ee5b62c6e4bff862e8fa1c8632bf4f6f24c58
dariusehssan <dariusehssan@gmail.com>
2026-08-11 21:59:58 +0100

    Update LabelList.jsx

--- frontend/src/LabelList.jsx
+++ frontend/src/LabelList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Labels.css";
 
-const API_URL = import.meta.env.REACT_APP_API_URL || "http://127.0.0.1:5000";
+const API_URL = import.meta.env.VITE_API_URL || "https://data-to-graph-web-automator.onrender.com";
 
 const LabelList = ({ labels, updateLabel, updateCallback, selectedPresetId, setSelectedPresetId }) => {
     const onDelete = async (id) => {
