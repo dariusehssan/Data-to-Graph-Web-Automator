93c91b3b44bdb4eaf6302900b39c462dfe934ea2
dariusehssan <dariusehssan@gmail.com>
2026-09-22 19:36:41 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pyodbc
-import os
 from Classes import DBConnectionManager, GraphPreset, DataUploader, GraphBuilder
 
 app = Flask(__name__)
